d31729a366946ce4aae6c08013e17a30483a68d2
M1ndst0rM <mountan_spirit-github@yahoo.de>
2021-06-13 17:20:52 +0200

    Implement grid snapping of instruments

--- Unity Project/GMTK_GJ2021/Assets/Scripts/Instruments/Instrument.cs
+++ Unity Project/GMTK_GJ2021/Assets/Scripts/Instruments/Instrument.cs
@@ -21,11 +21,34 @@ namespace Instruments
         private void Start()
         {
             InstrumentManager.Instance.RegisterInstrument(this);
+            SnapToNearestTile();
         }
 
         private void OnDestroy()
         {
             InstrumentManager.Instance.UnregisterInstrument(this);
+        }
+
+        public void SnapToNearestTile()
+        {
+            var tiles = GameObject.FindGameObjectsWithTag("tiles");
+            var shortestDistance = float.MaxValue;
+            GameObject closestTile = null;
+
+            foreach (var tile in tiles)
+            {
+                var distance = Vector3.Distance(tile.transform.position, transform.position);
+                if (distance < shortestDistance)
+                {
+                    closestTile = tile;
+                    shortestDistance = distance;
+                }
+            }
+
+            if (closestTile != null)
+            {
+                transform.position = closestTile.transform.position;
+            }
         }
     }
 }
